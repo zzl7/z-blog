b8cc25a3373b2cb691d19300df5b58367c5fba0b
ziliang.zhou <ziliang.zhou@youruncloud.com>
2018-03-01 17:59:44 +0800

    增加首页

---
+++ front/src/pages/index/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card } from 'antd';
+const { Meta } = Card;
+import './index.less';
+class index extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    blogDetail() {
+        this.props.history.push('/');
+    }
+    render() {
+        return (
+            <div className="">
+                {
+                    [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(record => {
+                        return (
+                            <Card
+                                hoverable
+                                key={record}
+                                onClick={this.blogDetail.bind(this)}
+                                className="mycard"
+                                cover={<img alt="example" src="https://alpha.wallhaven.cc/wallpapers/thumb/small/th-479801.jpg" />}
+                            >
+                                <Meta
+                                    title="网页设计趋势"
+                                    description="网页设计师不要错过！2015年度最值得关注的21个网页设计趋势"
+                                />
+                            </Card>
+                        );
+                    })
+                }
+            </div>
+            
+        )
+    }
+}
+export default index
--- front/src/pages/layout/index.jsx
+++ front/src/pages/layout/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import { Card } from 'antd';
 const { Meta } = Card;
 import './index.less';
+import IndexPage from '../index'
+import history from 'history/createBrowserHistory';
+
 class index extends React.Component {
     constructor(props){
         super(props)
@@ -55,28 +58,13 @@ class index extends React.Component {
                 <div className="body-content">
                     {/* <div className="left">header</div> */}
                     <div className="center">
-                        {
-                            [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(record => {
-                                return (
-                                    <Card
-                                        hoverable
-                                        key={record}
-                                        onClick={this.blogDetail.bind(this)}
-                                        className="mycard"
-                                        cover={<img alt="example" src="https://alpha.wallhaven.cc/wallpapers/thumb/small/th-479801.jpg" />}
-                                    >
-                                        <Meta
-                                            title="网页设计趋势"
-                                            description="网页设计师不要错过！2015年度最值得关注的21个网页设计趋势"
-                                        />
-                                    </Card>
-                                );
-                            })
-                        }
-
+                         <Route path="/index" component={IndexPage} />
                     </div>
                     {/* <div className="right">right</div> */}
                 </div>
+                <footer className="footer">
+                    
+                </footer> 
             </div>
         )
     }
