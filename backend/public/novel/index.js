ef9cd47be51fda08bc5ef86caa49f9aaf925f8db
ziliang.zhou <ziliang.zhou@youruncloud.com>
2018-04-20 11:44:20 +0800

    小说

--- backend/public/novel/index.js
+++ backend/public/novel/index.js
@@ -1,56 +1,4 @@
-// $.ajax({
-//     url: '/book/by-categories?gender=male&type=hot&major=%E5%A5%87%E5%B9%BB&minor=&start=0&limit=20',
-//     type: 'GET',
-//     success: function (res) {
-//         var html = '';
-//         for (var i = 0; i < res.books.length; i++) {
-//             html += '<li data-id="' + res.books[i]._id + '"><a href="#">' + res.books[i].title + '</a></li>'
-//         }
-//         $("#novel").html(html);
 
-//         $("#novel li").click(function () {
-//             // var id = $(this).attr('data-id');
-//             var id = $(this).attr('data-id');
-
-//             $.ajax({
-//                 url: '/atoc?view=summary&book=' + id,
-//                 type: 'GET',
-//                 success: function (data) {
-//                     var bookid = '5acb34a6e36bd0e811d3106b';
-//                     // for (var j = 0; j < data.length; j++) {
-//                     //     if (data[j].source == 'biquge') {
-//                     //         bookid = data[j]._id
-//                     //     }
-//                     // }
-//                     $.ajax({
-//                         url: '/atoc/' + bookid + '?view=chapters',
-//                         type: 'GET',
-//                         success: function (chapters) {
-//                             var chaptersHtml = '';
-//                             for (var i = 0; i < chapters.chapters.length; i++) {
-//                                 chaptersHtml += '<li data-link="' + chapters.chapters[i].link + '"><a href="#">' + chapters.chapters[i].title + '</a></li>'
-//                             }
-//                             $("#chapters").html(chaptersHtml);
-//                             $("#chapters li").click(function () {
-//                                 var link = $(this).attr('data-link');
-//                                 $.ajax({
-//                                     url: '/chapter/' + link,
-//                                     type: 'GET',
-//                                     success: function (content) {
-//                                         $("#chapterTitle").html(content.chapter.title);
-//                                         $("#content").html(content.chapter.body);
-//                                     }
-//                                 })
-//                             })
-//                         }
-//                     })
-
-//                 }
-//             })
-//         })
-//     }
-// })
-//凡人修仙传
 $(function () {
     $("#searchBtn").click(function () {
         var keyword = $("#search").val();
@@ -64,7 +12,7 @@ $(function () {
                 for (var i = 0; i < res.bookList.length; i++) {
                     html += '<li data-link="' + res.bookList[i].link + '">';
                     html += '<div class="book-name">' + res.bookList[i].name + '</div>';
-                    html += '<div >作者: ' + res.bookList[i].author + '</div>';
+                    html += '<div >' + res.bookList[i].author + '</div>';
                     html += '<div >最新章节: ' + res.bookList[i].updateName + '</div>';
                     html += '</li>'
                 }
